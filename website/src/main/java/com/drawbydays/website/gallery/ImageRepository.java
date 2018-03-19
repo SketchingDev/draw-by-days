8d38efd8433d2b11ebc2b1bcc032e13a436e306c
SketchingDev <SketchingDev@users.noreply.github.com>
2018-03-19 20:16:25 +0000

    Simplify repository method for finding next image

--- website/src/main/java/com/drawbydays/website/gallery/ImageRepository.java
+++ website/src/main/java/com/drawbydays/website/gallery/ImageRepository.java
@@ -8,24 +8,17 @@ public interface ImageRepository extends Repository<Image, Long> {
 
   Optional<Image> findById(Long id);
   Optional<Image> findFirstByOrderByIdAsc();
-  Iterable<Image> findAllByOrderByIdAsc();
+  Optional<Image> findByIdGreaterThanOrderByIdAsc(Long id);
+  void save(Iterable<Image> entities);
   Image save(Image owner);
-  Iterable<Image> save(Iterable<Image> entities);
 
   default Optional<Image> findNextElseFirstByOrderByIdAsc(Long id) {
-    final Iterable<Image> images = this.findAllByOrderByIdAsc();
+    final Optional<Image> image = findByIdGreaterThanOrderByIdAsc(id);
 
-    Image firstImage = null;
-    for (final Image image : images) {
-      if (firstImage == null) {
-        firstImage = image;
-      }
-
-      if (image.getId() > id) {
-        return Optional.of(image);
-      }
+    if (image.isPresent()) {
+      return image;
+    } else {
+      return findFirstByOrderByIdAsc();
     }
-
-    return Optional.ofNullable(firstImage);
   }
 }
