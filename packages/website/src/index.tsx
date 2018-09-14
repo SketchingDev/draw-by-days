bff5f0664445e7f0b70850833907a2e01f60f0d9
Lucas <31957045+SketchingDev@users.noreply.github.com>
2018-09-14 15:01:57 +0100

    Change base project to use Material UI
    Example was taken from:
    https://github.com/mui-org/material-ui/tree/master/examples/create-react-app-with-typescript

---
+++ packages/website/src/index.tsx
@@ -0,0 +1,12 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import { Hello } from "./components/Hello";
+
+// const rootComponent = <Hello compiler="TypeScript" framework="React" />
+// ReactDOM.render(rootComponent, document.getElementById("root"));
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Index from './pages/index';
+
+ReactDOM.render(<Index />, document.querySelector('#root'));
