5ad271c069fd3b956e3a1942779a906c3fcb3349
SketchingDev <SketchingDev@users.noreply.github.com>
2018-11-15 12:43:43 +0000

    Remove Image API lambda
    Remove the proxy lambda that would serve up images, as it is now
    redundent and the API Gateway proxies requests straight to DynamoDB.

--- packages/image-api/src/handler/returnImage.ts
+++
@@ -1,9 +0,0 @@
-import { IDailyImage } from "image-lib";
-
-export const getImage = async (): Promise<IDailyImage> => Promise.resolve({
-    date: new Date(Date.now()).toLocaleString(),
-    image: {
-        description: "Hello Worlddd",
-        path: "https://www.w3.org/People/mimasa/test/imgformat/img/w3c_home.jpg",
-    },
-});
