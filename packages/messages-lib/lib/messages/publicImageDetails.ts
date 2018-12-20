e5d68f5b4d408350d547816a4b66257f10ac11ba
Lucas <31957045+SketchingDev@users.noreply.github.com>
2018-12-20 17:15:52 +0000

    Allow CI pipeline to run docker commands

--- packages/messages-lib/lib/messages/publicImageDetails.ts
+++ packages/messages-lib/lib/messages/publicImageDetails.ts
@@ -1,16 +1,16 @@
 export interface IImageDimensions {
-    width: number;
-    height: number;
+  width: number;
+  height: number;
 }
 
 export interface IPublicImage {
-    publicUrl: string;
-    dimensions: IImageDimensions;
+  publicUrl: string;
+  dimensions: IImageDimensions;
 }
 
 // TODO Should this only relate to a public image URL being available? If so I should remove the description.
 // TODO Otherwise if this just pertains to the details then I should remove the images
 export interface IPublicImageDetails {
-    description: string;
-    images: [IPublicImage];
+  description: string;
+  images: [IPublicImage];
 }
