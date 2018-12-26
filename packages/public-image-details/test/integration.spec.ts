6319f4992cb0f2760ff5c17c929f1b1d7b3ac2ce
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2018-12-26 18:20:45 +0000

    Simplify integration test

--- packages/public-image-details/test/integration.spec.ts
+++ packages/public-image-details/test/integration.spec.ts
@@ -4,7 +4,7 @@ import AWS from "aws-sdk";
 import axios from "axios";
 import waitForExpect from "wait-for-expect";
 
-const uuidv4 = require("uuid/v4");
+import uuidv4 from "uuid/v4";
 
 const defaultTimeout = 5000;
 jest.setTimeout(defaultTimeout * 4);
@@ -32,19 +32,6 @@ describe("Public Image Details integration test", () => {
     let result;
     await waitForExpect(async () => {
       result = await axios.get(`${process.env.TF_OUTPUT_private_url}/${imageId}`);
-
-      // Retry until value unique to the test to propagates to the data-store
-      expect(result).toMatchObject({
-        data: {
-          Items: [
-            {
-              Description: {
-                S: description,
-              },
-            },
-          ],
-        },
-      });
     });
 
     expect(result).toMatchObject({
