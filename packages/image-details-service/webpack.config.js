eb68177e35da61acce608b8b7658ac993d618a27
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2019-01-16 20:47:39 +0000

    Update webconfig to produe two lambdas

--- packages/image-details-service/webpack.config.js
+++ packages/image-details-service/webpack.config.js
@@ -1,47 +1,34 @@
 const ZipPlugin = require("zip-webpack-plugin");
 
-// https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
-
-const commonConfig = {};
+const commonConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].js",
+    libraryTarget: "commonjs",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  target: "node",
+  externals: ["aws-sdk"],
+  module: {
+    rules: [{ test: /\.tsx?$/, use: ["ts-loader"] }],
+  },
+};
 
 const saveImageDetails = filename =>
   Object.assign({}, commonConfig, {
-    mode: "production",
     entry: "./src/saveImageDetails/main.ts",
-    output: {
-      filename: "[name].js",
-      libraryTarget: "commonjs",
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"],
-    },
-    target: "node",
-    externals: ["aws-sdk"],
-    module: {
-      rules: [{ test: /\.tsx?$/, use: ["ts-loader"] }],
-    },
     plugins: [new ZipPlugin({ filename })],
   });
 
 const saveImageStore = filename =>
   Object.assign({}, commonConfig, {
-    mode: "production",
     entry: "./src/saveImageStore/main.ts",
-    output: {
-      filename: "[name].js",
-      libraryTarget: "commonjs",
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"],
-    },
-    target: "node",
-    externals: ["aws-sdk"],
-    module: {
-      rules: [{ test: /\.tsx?$/, use: ["ts-loader"] }],
-    },
     plugins: [new ZipPlugin({ filename })],
   });
 
-module.exports = env => {
-  return [saveImageDetails(env.save_image_details_filename), saveImageStore(env.save_image_store_filename)];
-};
+module.exports = env => [
+  saveImageDetails(env.save_image_details_filename),
+  saveImageStore(env.save_image_store_filename),
+];
