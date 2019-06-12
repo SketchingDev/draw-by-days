89b9c04196162197fec803ee96eac614385da2b8
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2019-06-12 21:55:44 +0100

    Add CORS header to lambda response

--- packages/daily-image-api/app/query/http/apiGatewayAdapter.ts
+++ packages/daily-image-api/app/query/http/apiGatewayAdapter.ts
@@ -4,17 +4,22 @@ import { App } from "../app";
 
 const { res } = require("@laconia/event").apigateway;
 
-// https://github.com/laconiajs/laconia/blob/6af929d1dc227e59b2f8094b38b7dd5e7cd78434/packages/laconia-acceptance-test/src/accept-order.js
-const withCors = (next: any) => async (...args: any[]) => {
-  const response = await next(...args);
-  // TODO Update wildcard to be host in request, so long as it matches *.drawbydays.com
-  response.headers["Access-Control-Allow-Origin"] = "*";
+const domain = "drawbydays.com";
+const defaultOrigin = `http://${domain}`;
+
+const withCors = (next: any) => async (event: APIGatewayProxyEvent, ...args: any[]) => {
+  const response = await next(event, ...args);
+
+  const origin = event.headers && event.headers.origin;
+  const isOriginDrawByDays = origin && origin.endsWith(domain);
+
+  response.headers["Access-Control-Allow-Origin"] = isOriginDrawByDays ? origin : defaultOrigin;
+
   return response;
 };
 
 export const apiGatewayAdapter = (app: App) =>
   withCors(async (_: APIGatewayProxyEvent, dependencies: AppDependencies) => {
-    console.log(_);
     try {
       const output = await app(new Date(), dependencies);
       return res(output);
